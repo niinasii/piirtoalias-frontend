910c1b84073277e22ed4acf546d669823e8bdc47
jaakko koskela <jaska.koskela@gmail.com>
2020-03-23 15:13:47 +0200

    websocketit ja eka versio chatista

--- src/components/ChatViesti.js
+++ src/components/ChatViesti.js
@@ -1,1 +1,43 @@
-//Chätti viestit
+import React, { Component } from 'react'
+import { TextInput, Text, View } from 'react-native'
+import io from 'socket.io-client'
+
+export default class ChatViesti extends Component {
+    constructor(props)  {
+        super(props);
+        this.state = {
+            chatMessage: "",
+            chatMessages: []
+        };
+    }
+
+componentDidMount () {
+    this.socket = io('http://192.168.1.5:3000/')
+    this.socket.on("chat message", msg  => {
+        this.setState({chatMessages: [...this.state.chatMessages, msg]})
+    })
+}
+
+submitChatMessage() {
+this.socket.emit('chat message', this.state.chatMessage)
+this.setState({ chatMessage: "" })
+}
+
+    render() {
+    const chatMessages = this.state.chatMessages.map(chatMessage => <Text key={chatMessage}>{chatMessage}</Text>)
+        return (
+            <View>
+
+                <TextInput 
+                style={{height: 40, borderWidth: 2}}
+                autoCorrect={false}
+                value={this.state.chatMessage}
+                onSubmitEditing={()=> this.submitChatMessage()}
+                onChangeText={chatMessage => {
+                    this.setState({chatMessage})
+                }}/>
+                {chatMessages}
+            </View>
+        )
+    }
+}
